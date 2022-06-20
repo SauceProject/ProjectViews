701718c24a3e2ffb92eab79e709c303ae8da4f86
BasmaEsmail <basmaabas755@gmail.com>
2022-06-20 16:31:42 +0200

    add MVC

---
+++ ITI.Sauce.Models/ITI.Sauce.MVC/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static int Main()
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.Services.AddControllersWithViews();
+        var app = builder.Build();
+        app.MapDefaultControllerRoute();
+        app.Run();
+        return 0;
+    }
+}
